b28e48c58124904d29d760c0f3db33d3b0b7403a
mahiuddiinnTuhin <mahiuddiinntuhin@gmail.com>
2023-04-18 08:51:24 +0600

    testing

--- index.js
+++ index.js
@@ -33,19 +33,51 @@ const client = new MongoClient(uri, {
 /* mongodb operation */
 
 async function run() {
+  const teacherDataCollection = client.db("guidant").collection("teachersData");
+  const appointingCollection = client
+    .db("guidant")
+    .collection("appointingData");
   try {
     /* **********************************/
     /* CRUD operation of teacher's Data */
     /* **********************************/
-    const teacherDataCollection = client
-      .db("guidant")
-      .collection("teachersData");
 
     /* get all data */
     app.get("/teachersData", async (req, res) => {
       const query = {}; //search query
       const response = await teacherDataCollection.find(query).toArray();
       res.send(response);
+    });
+    /* get all data ends*/
+
+    /* get single data */
+    app.get("/singleTeacher/", async (req, res) => {
+      const id = req.query.id;
+
+      const query = { id: id }; //search query
+      const response = await teacherDataCollection.findOne(query);
+      res.send(response);
+    });
+    /* get single data ends*/
+
+    /* **********************************/
+    /* CRUD operation of appointing Data */
+    /* **********************************/
+
+    /* get all data */
+    app.get("/appointingData", async (req, res) => {
+      const query = {}; //search query
+      const response = await appointingCollection.find(query).toArray();
+      res.send(response);
+    });
+    /* get all data ends*/
+
+    /* get all data */
+    app.post("/postAppointing", async (req, res) => {
+      const doc = req.body;
+
+      const response = await appointingCollection.insertOne(doc);
+      console.log(`A document was inserted with the _id: ${doc?.id}`);
     });
     /* get all data ends*/
   } catch (error) {
